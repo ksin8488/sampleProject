160425e5aa22a41b1f6e619bf8d975911228924a
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-01-25 13:44:17 -0700

    Finished the Console Output assignment and started using pointers.
    Also added comments for clarification on certain sections.

--- sampleProject/Controller/Controller.cpp
+++ sampleProject/Controller/Controller.cpp
@@ -17,16 +17,33 @@ Controller :: Controller()  //constructor - class, scope resolution operator, me
 
 void Controller :: start()
 {
-    for (int index = 0; index < 10; index++)
+    for (int index = 0; index < 10; index++)    //for loop to create 10 numbers that are printed to the screen
     {
         cout << "This is the " << index + 1 << " time in the loop" << endl;
     }
     
+    //Displays & calls the differnt functions for the arrays
     cout << practiceArray() << endl;
     cout << doubleArray() << endl;
-    cout << stringArray() << endl;
+    cout << stringArray() << endl << endl;
+    
+    //Console Output Assignment
+    string name = "Kashish Singh";
+    cout << name << endl;
+    
+    for (int i = 0; i < 28; i++)
+    {
+        cout << i + 12 << " " << " from 13-31. " << endl;
+    }
+    
+    string *nameAddress = &name;
+    cout << "Pointer for my name is, " << nameAddress << endl << endl;
+    //cout << &name << endl;        - used to double check if the addresses were actually the same
+    
 }
 
+
+//Array Assignment
 int Controller :: practiceArray()
 {
     int array[5] = {0, 1, 2, 3, 4};
